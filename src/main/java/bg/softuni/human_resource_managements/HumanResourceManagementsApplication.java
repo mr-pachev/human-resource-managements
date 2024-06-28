0d297ab4e5ec1ee503ae2271c1a1674977df97fc
Ivan Pachev <mr.pachev@abv.bg>
2024-06-28 23:06:27 +0300

    Creat skeleton human resources managements

---
+++ src/main/java/bg/softuni/human_resource_managements/HumanResourceManagementsApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.human_resource_managements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceManagementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanResourceManagementsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/softuni/human_resource_managements/HumanResourceManagementsApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.human_resource_managements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceManagementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
