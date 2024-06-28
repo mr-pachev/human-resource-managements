84bbc0e2a000a3d243085f3804fd4b6b31c3e984
Ivan Pachev <mr.pachev@abv.bg>
2024-06-28 23:50:39 +0300

    Creat BaseEntity

---
+++ src/main/java/bg/softuni/human_resource_managements/model/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package bg.softuni.human_resource_managements.model.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    protected BaseEntity() {
+    }
+
+    public long getId() {
+        return id;
+    }
+}
