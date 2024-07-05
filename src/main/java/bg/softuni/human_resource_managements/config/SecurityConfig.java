8076cc2ac09ab25e653bff5a1b929587c0a515b5
Ivan Pachev <mr.pachev@abv.bg>
2024-07-05 11:43:37 +0300

    Added SecurityConfig in config folder.

---
+++ src/main/java/bg/softuni/human_resource_managements/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package bg.softuni.human_resource_managements.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                        authorizeRequest -> {
+                            authorizeRequest
+                                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                    .requestMatchers("/login", "/registration").permitAll()
+                                    .anyRequest().authenticated();
+                        }
+                )
+                .formLogin(
+                        formLogin -> {
+                            formLogin.loginPage("/login");
+                            formLogin.usernameParameter("username");
+                            formLogin.passwordParameter("password");
+                            formLogin.defaultSuccessUrl("/", true);
+                            formLogin.failureUrl("/login-error");
+                        }
+                )
+                .logout(
+                        logout -> {
+                            logout.logoutUrl("/logout");
+                            logout.logoutSuccessUrl("/");
+                            logout.invalidateHttpSession(true);
+                        }
+                ) //страница за проверка на идентификация(authentication -> кой)
+                .build();
+    }
+
+}
