e3cb0772287de743622a76f33b1176353d9abb05
Ivan Pachev <mr.pachev@abv.bg>
2024-07-07 14:05:50 +0300

    Added employees template.

---
+++ src/main/java/bg/softuni/human_resource_managements/model/dto/EmployeeDTO.java
@@ -0,0 +1,94 @@
+package bg.softuni.human_resource_managements.model.dto;
+
+public class EmployeeDTO {
+
+    private String firstName;
+
+    private String middleName;
+
+    private String lastName;
+
+    private String identificationNumber;
+
+    private int age;
+
+    private String startDate;
+
+    private String position;
+
+    private String department;
+
+    private String education;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public void setIdentificationNumber(String identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+}
