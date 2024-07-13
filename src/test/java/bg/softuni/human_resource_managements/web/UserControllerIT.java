72a8770f49601bdc4ec6d21c8fc79c498eb89084
Ivan Pachev <mr.pachev@abv.bg>
2024-07-13 18:04:20 +0300

    Fixed logout button translate.

---
+++ src/test/java/bg/softuni/human_resource_managements/web/UserControllerIT.java
@@ -0,0 +1,79 @@
+package bg.softuni.human_resource_managements.web;
+
+import bg.softuni.human_resource_managements.model.entity.User;
+import bg.softuni.human_resource_managements.repository.UserRepository;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerIT {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private WireMockServer wireMockServer;
+
+    @BeforeEach
+    public void setup() {
+        wireMockServer = new WireMockServer(WireMockConfiguration.options().port(8081));
+        wireMockServer.start();
+
+        wireMockServer.stubFor(get(urlPathEqualTo("/employees"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("[{\"identificationNumber\": \"2111111112\", \"name\": \"Test Employee\"}]")));
+    }
+
+    @AfterEach
+    public void teardown() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    void testRegistrationUser() throws Exception {
+        mockMvc.perform(post("/registration")
+                        .param("username", "test")
+                        .param("identificationNumber", "2111111112")
+                        .param("password", "0000")
+                        .param("confirmPassword", "0000")
+                        .param("role", "ADMIN")
+                        .with(csrf())
+                ).andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/registration"));
+
+        Optional<User> userEntityOpt = userRepository.findByUsername("test");
+
+        Assertions.assertTrue(userEntityOpt.isPresent());
+
+        User foundCreatUser = userEntityOpt.get();
+
+        Assertions.assertEquals("test", foundCreatUser.getUsername());
+        Assertions.assertEquals("2111111112", foundCreatUser.getIdentificationNumber());
+        Assertions.assertTrue(passwordEncoder.matches("0000", foundCreatUser.getPassword()));
+        Assertions.assertEquals("ADMIN", foundCreatUser.getRole().getRoleName().name());
+    }
+}
+
