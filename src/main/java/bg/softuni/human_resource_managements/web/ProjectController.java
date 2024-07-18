8b030f8d0de2df1c9df07d0450b5649f8e162abd
Ivan Pachev <mr.pachev@abv.bg>
2024-07-18 16:45:38 +0300

    Improved edit project request.

--- src/main/java/bg/softuni/human_resource_managements/web/ProjectController.java
+++ src/main/java/bg/softuni/human_resource_managements/web/ProjectController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -37,9 +38,12 @@ public class ProjectController {
     }
 
     @PostMapping("/project-details")
-    public String editProject(@Valid ProjectDTO projectDTO,
+    public String editProject(@RequestParam("id") Long id,
+                                @Valid ProjectDTO projectDTO,
                               BindingResult bindingResult,
                               RedirectAttributes rAtt) {
+
+        projectDTO.setId(id);
 
         if (bindingResult.hasErrors()) {
             rAtt.addFlashAttribute("projectDTO", projectDTO);
