6d161dcf9ba2b480090d1d9287a723c1b30bba05
Ivan Pachev <mr.pachev@abv.bg>
2024-07-27 13:17:21 +0300

    Sorted DepartmentService.

--- src/main/java/bg/softuni/human_resource_managements/service/DepartmentService.java
+++ src/main/java/bg/softuni/human_resource_managements/service/DepartmentService.java
@@ -5,22 +5,29 @@ import bg.softuni.human_resource_managements.model.dto.*;
 import java.util.List;
 
 public interface DepartmentService {
+    //get all departments names
     List<String> getAllDepartments();
-
+    //get all departments
     List<DepartmentDTO> getAllDepartmentsDTOS();
-    List<DepartmentEmployeeDTO> getAllEmployeesNames();
-
-    List<EmployeeDTO> allDepartmentEmployees(long id);
-
-    DepartmentDTO getDepartmentDTOByID(long id);
-
-    void editDepartment(DepartmentDTO departmentDTO);
-
-    void addDepartment(AddDepartmentDTO addDepartmentDTO);
+    //checking is exist department by name
     boolean isExistDepartment(String name);
-    boolean isExistEmployeeInDepartment(String employeeName, long idDep);
+    //add new department
+    void addDepartment(AddDepartmentDTO addDepartmentDTO);
+    //get department
+    DepartmentDTO getDepartmentDTOByID(long id);
+    //edit department
+    void editDepartment(DepartmentDTO departmentDTO);
+    //delete department
+    void removeDepartment(long id);
 
+
+    //get all employees names from current department
+    List<DepartmentEmployeeDTO> getAllEmployeesNames();
+    //get all employees from current department
+    List<EmployeeDTO> allDepartmentEmployees(long id);
+    //checking is exist current employee in current department
+    boolean isExistEmployeeInDepartment(String employeeName, long idDep);
+    //add employee in current department
     void addDepartmentEmployee(DepartmentEmployeeDTO departmentEmployeeDTO, long idDep);
 
-    void removeDepartment(long id);
 }
