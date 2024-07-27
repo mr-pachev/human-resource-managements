dc7e628d9a3f7dde6784c09f671588bc27e4f6d1
Ivan Pachev <mr.pachev@abv.bg>
2024-07-27 15:53:38 +0300

    Sorted ProjectService.

--- src/main/java/bg/softuni/human_resource_managements/service/ProjectService.java
+++ src/main/java/bg/softuni/human_resource_managements/service/ProjectService.java
@@ -5,15 +5,36 @@ import bg.softuni.human_resource_managements.model.dto.*;
 import java.util.List;
 
 public interface ProjectService {
-    boolean addProject(AddProjectDTO addProjectDTO);
-    boolean isExistProject(String projectName);
+    //get all projects
     List<ProjectDTO> getAllProjectsDTOS();
-    List<ProjectEmployeeDTO> getAllEmployeesNames();
+
+    //checking is exist project by name
+    boolean isExistProject(String projectName);
+
+    //add new project
+    boolean addProject(AddProjectDTO addProjectDTO);
+
+    //get project by id
     ProjectDTO getProjectDTOByID(long id);
-    void removeEmployeeFromProject(long idEm, long idPr);
-    void removeProject(long id);
-    List<EmployeeDTO> allProjectEmployees(long id);
+
+    //edit project
     void editProject(ProjectDTO projectDTO);
-    void addProjectEmployee(ProjectEmployeeDTO projectEmployeeDTO, long idPr);
+
+    //delete project
+    void removeProject(long id);
+
+    //get all employees from current project
+    List<EmployeeDTO> allProjectEmployees(long id);
+
+    //get all employees names
+    List<ProjectEmployeeDTO> getAllEmployeesNames();
+
+    //checking is exist current employee in current project
     boolean isExistEmployeeInProject(String employeeName, long idPr);
+
+    //add current employee in current project
+    void addProjectEmployee(ProjectEmployeeDTO projectEmployeeDTO, long idPr);
+
+    //delete current employee from current project
+    void removeEmployeeFromProject(long idEm, long idPr);
 }
