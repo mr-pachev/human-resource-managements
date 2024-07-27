ff539668c60e7bbf0c0bd5f5a9745eaef5d5551e
Ivan Pachev <mr.pachev@abv.bg>
2024-07-27 15:37:05 +0300

    Sorted PositionService.

--- src/main/java/bg/softuni/human_resource_managements/service/PositionService.java
+++ src/main/java/bg/softuni/human_resource_managements/service/PositionService.java
@@ -5,20 +5,36 @@ import bg.softuni.human_resource_managements.model.dto.*;
 import java.util.List;
 
 public interface PositionService {
-   //get all
+   //get all positions names
    List<String> getAllPositionNames();
-   List<PositionDTO> getAllPositionsDTOS();
-   List<EmployeeDTO> allPositionEmployees(long id);
 
-   //position
-   void addPosition(AddPositionDTO addPositionDTO);
-   PositionDTO getPositionDTOByID(long id);
+   //get all positions
+   List<PositionDTO> getAllPositionsDTOS();
+
+   //checking is exist position by name
    boolean isExistPosition(String name);
+
+   //add new position
+   void addPosition(AddPositionDTO addPositionDTO);
+
+   //get position by id
+   PositionDTO getPositionDTOByID(long id);
+
+   //edit position
    void editPosition(PositionDTO positionDTO);
+
+   //delete position
    void removePosition(long id);
 
-   //position employees
-   void addPositionEmployee(PositionEmployeesDTO positionEmployeesDTO, long idPos);
+   //get all employees from current position
+   List<EmployeeDTO> allPositionEmployees(long id);
+
+   //checking is exist employee in current position
    boolean isExistEmployeeInPosition(String employeeName, long idPos);
+
+   //add current employee in current position
+   void addPositionEmployee(PositionEmployeesDTO positionEmployeesDTO, long idPos);
+
+   //delete current employee from current position
    void removeEmployeeFromPosition(long idEm, long idPos);
 }
